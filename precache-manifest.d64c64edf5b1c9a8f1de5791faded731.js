912fc5d2859cc7240413ec8a353304785cd6c283
Lucas Lima <lucastlima86@gmail.com>
2020-01-30 19:22:17 +0000

    Updates

---
+++ precache-manifest.d64c64edf5b1c9a8f1de5791faded731.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6389f0398f14ac8c6e0649947c2bf381",
+    "url": "/moove/index.html"
+  },
+  {
+    "revision": "b95e62ad321ade1f796c",
+    "url": "/moove/static/js/2.1f22db33.chunk.js"
+  },
+  {
+    "revision": "7eb906b2d8ef627da08d7f4a543e6732",
+    "url": "/moove/static/js/2.1f22db33.chunk.js.LICENSE"
+  },
+  {
+    "revision": "786d353c2f003951f532",
+    "url": "/moove/static/js/main.f10c9044.chunk.js"
+  },
+  {
+    "revision": "3346e1415efa3654775c",
+    "url": "/moove/static/js/runtime-main.44c81e4d.js"
+  },
+  {
+    "revision": "2d3091a2cbb00e15ae38fdd57cc06ca1",
+    "url": "/moove/static/media/loader.2d3091a2.svg"
+  },
+  {
+    "revision": "2d0a94e2f633336938bd95b242ea431b",
+    "url": "/moove/static/media/placeholder.2d0a94e2.svg"
+  }
+]);
